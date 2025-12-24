b20d32d4490024c61c509d1787866248f46dc47c
Limi <126730851+Limi3@users.noreply.github.com>
2025-12-24 17:13:34 +0100

    updating project

--- React Project/src/data/flagImages.js
+++
@@ -1,8 +0,0 @@
-const flagImages = {
-    'assets/flags/belgium.png': require('../../assets/flags/belgium.png'),
-    'assets/flags/albania.png': require('../../assets/flags/albania.png'),
-    'assets/flags/canada.png': require('../../assets/flags/canada.png'),
-    'assets/flags/kosovo.png': require('../../assets/flags/kosovo.png'),
-};
-
-export default flagImages;
